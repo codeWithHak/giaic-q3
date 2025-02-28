34ec3a1350af649701101a99fd54b5122316ff0d
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-02-28 13:08:04 +0500

    changed syntax to use model removed client

--- h-gpt/app.py
+++ h-gpt/app.py
@@ -1,22 +1,18 @@
 # importing genai a library which will allow us to use gemini model
-from google import genai # type: ignore
+import google.generativeai as genai # type: ignore
 import streamlit as st
 import os # is is imported to use environment variables
 
 st.title("Welcome to H-Gpt")
 st.header("Lil bro of ChatGpt ;)")
-api_key = os.environ.get("GOOGLE_API_KEY")
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # function that will call the api
-def get_gemini_response(user_input,api_key):
-    if api_key is None:
-        return "APi key issue"
-
-    else:
-        client = genai.Client(api_key=api_key)
-        response = client.models.generate_content(
-        model = "gemini-2.0-flash", contents=user_input)
-        return response.text
+def get_gemini_response(user_input):    
+    model = genai.GenerativeModel('gemini-2.0-flash')
+    response = model.generate_content(user_input)
+    return response.text
 
 
 #taing input from the user
@@ -28,8 +24,11 @@ response_placeholder = st.empty()
 if user_input:
     # with will automatically stops the spinner when function will be executed
     with st.spinner("Generating response..."):
-        response_text = get_gemini_response(user_input,api_key)
+        response_text = get_gemini_response(user_input)
         response_placeholder.write(response_text)
 
 
 # print(response.text)
+
+for model in genai.list_models():
+    print(model)
