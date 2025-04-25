630dc9dc826eca3227a86e5a9ed7555f94888c0e
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-04-25 18:15:45 +0500

    improve average method

--- python/udemy_course/10-oop/projects/01_methods.py
+++ python/udemy_course/10-oop/projects/01_methods.py
@@ -1,14 +1,12 @@
 class Student:
-    def __init__(self,name,english,urdu,islamiat):
+    def __init__(self,name,marks):
         self.name = name
-        self.english = english
-        self.urdu = urdu
-        self.islamiat = islamiat
+        self.marks = marks
         
     def average(self):
-        print((self.english + self.urdu + self.islamiat) / 3)
+        print(f"{sum(self.marks) / 3:.2f}")
         
-huzair = Student('Huzair',90,80,90)
+huzair = Student('Huzair',[90,80,90])
 print(huzair)
 
 huzair.average()
