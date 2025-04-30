ba972054ee0458d8912ed46b3d2fd65ceca759c5
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-04-30 16:02:19 +0500

    complete hangman

--- python/projects/04-assignments/projects_to_be_submitted_by_students/05_hangman.py
+++
@@ -1,7 +0,0 @@
-# ALGO: 
-# 1- Computer should pick a word for us to guess, for that we have a list of words from which the computer can pick from.
-# 1.1- Some words have dashes or spaces in them so we cant's choose them.
-# 1.2- Now we need to keep loopong over the word list to find a word that dosent have dash - or space " "
-
-def getValid_word():
-    
