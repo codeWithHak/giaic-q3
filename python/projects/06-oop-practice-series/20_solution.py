b7f9d76d321fea09cd440901f1b6eecb4bbcbd30
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-05-06 15:24:04 +0500

    complete solution 20

--- python/projects/06-oop-practice-series/20_solution.py
+++ python/projects/06-oop-practice-series/20_solution.py
@@ -1,18 +1,20 @@
-# Create a custom exception InvalidAgeError.
-# Write a function check_age(age) that raises this exception if age < 18.
-# Handle it with try...except.
+    # Create a custom exception InvalidAgeError.
+    # Write a function check_age(age) that raises this exception if age < 18.
+    # Handle it with try...except.
 
 class InvalidAgeError(Exception):
     
     def __init__(self,age):
         self.age = age
-        super().__init__(f"invalid age! Age must be in positive integers.")
+        super().__init__(f"Exception Raised: Invalid age! Age must be in positive integers.")
         
-def set_age(age):
-    if age > 0:
-        self.age = age
-        print(f"age is now {age}")
-    else:
-        raise InvalidAgeError(age)
-    
-set_age(-10)
+def check_age(age):
+        if age < 18:
+            raise InvalidAgeError(age)
+        else:
+            print(f"Allowed")
+            
+try:
+    check_age(-10)
+except InvalidAgeError as e:
+    print(e)
