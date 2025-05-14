9284db3ab073be85add3b5c0393a7e0042513f9b
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-05-14 08:08:18 +0500

    learn dunder methods

---
+++ python/udemy_course/12-oop-remastered/07_dunder_methods/new.py
@@ -0,0 +1,27 @@
+# duck typing
+class Bird:
+    
+    def __init__(self,name):
+        self.name = name
+    
+    def fly(self):
+        print(f"A {self.name} is Flying")
+        
+        
+class Plane:
+    
+    def __init__(self,name):
+        self.name = name
+    
+    def fly(self):
+        print(f"A {self.name} is Flying")
+        
+def check_duck(name):
+    return name.fly()
+
+check_duck(Bird("duck"))
+check_duck(Plane("J10C"))
+
+
+
+# __new__
